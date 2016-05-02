06d511e732693491b5f85e7a18bc15c1e2992ec3
talitamerces <talitamerces.m@gmail.com>
2016-05-02 17:14:19 -0300

    Teste

--- src/br/edu/ifba/mobile/padrao/cadeia/registraCar/Setor.java
+++ src/br/edu/ifba/mobile/padrao/cadeia/registraCar/Setor.java
@@ -3,15 +3,17 @@ package br.edu.ifba.mobile.padrao.cadeia.registraCar;
 public abstract class Setor {
 
 	protected Setor proximo = null;
+
 	
-	public void setProximoSetor(Setor proximo){
-		if(this.proximo != null){
+	//método
+	public void setProximoSetor(Setor proximo) {
+		if (this.proximo != null) {
 			this.proximo.setProximoSetor(proximo);
-		}else{
+		} else {
 			this.proximo = proximo;
 		}
+	
+	
 	}
-	
-	
-	
+
 }
